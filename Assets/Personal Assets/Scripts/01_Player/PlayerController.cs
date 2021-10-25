5431a5e127422714933835f6e9bc4f3dfada45a5
po5-tato28 <c2trillions@gmail.com>
2021-10-26 04:04:46 +0900

    Setting GameScene (2)
    Changing picture ratio

---
+++ Assets/Personal Assets/Scripts/01_Player/PlayerController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform groundDetector;
+    private Animator ani;
+    private bool isdead;
+    private float runlength;
+    private int jumpcount;
+
+    // Use this for initialization
+    void Awake()
+    {
+        groundDetector = transform.GetChild(0);
+        ani = gameObject.GetComponent<Animator>();
+        isdead = false;
+        runlength = 0.0f;
+        jumpcount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isdead) return;
+
+        runlength += Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetJump();
+        }
+    }
+
+    public void SetJump()
+    {
+        if (isdead) return;
+
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, groundDetector.position);
+
+        if (hit.transform != null)
+        {
+            jumpcount = 2;
+        }
+
+        if (Input.GetMouseButtonDown(0) && jumpcount > 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 300.0f));
+
+            jumpcount--;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ston")
+        {
+            coll.gameObject.SendMessage("ObjectHide");
+
+            ani.SetTrigger("dead");
+            isdead = true;
+
+            print("충돌했습니다");
+        }
+
+    }
+}
---
+++ Assets/Personal Assets/Scripts/08_Managers/Game/ObjectManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject enemy;
+    private float createtime;
+
+    private List<GameObject> Stonlist;
+    private float createtick;
+    //private GameObject[] StonArray;
+    // Use this for initialization
+    void Awake()
+    {
+        createtick = 0.0f;
+        createtime = 1.0f;
+        //StonArray
+        //Stonlist.Clear();
+        Stonlist = new List<GameObject>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject ston = Instantiate(enemy, Vector3.zero, Quaternion.identity) as GameObject;
+            ston.transform.parent = transform;
+            ston.SetActive(false);
+            Stonlist.Add(ston);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        createtick += Time.deltaTime;
+
+        if (createtick >= createtime)
+        {
+            createtime = Random.RandomRange(0.7f, 1.2f);
+
+            createtick = 0.0f;
+
+            foreach (GameObject st in Stonlist) //list array ตัดู ตส.
+            {
+                if (!st.activeSelf)
+                {
+                    st.transform.position = new Vector3(20.0f, -7.6f, 0.0f);
+                    st.SetActive(true);
+                    break;
+                }
+            }
+            /*
+            for (int i = 0; i < Stonlist.Count; i++)
+            {
+
+            }*/
+        }
+    }
+}
